cd808f9dd84ccfeec662a8f6b3f1b3e7b3754acf
Divya Siras <divyashiras976@gmail.com>
2026-06-19 11:13:12 +0530

    Update A3_Program8.py
    Python practice Assignment

--- A3_Program8.py
+++ A3_Program8.py
@@ -1,3 +1,8 @@
 # Write a function that does not return anything but prints a message. Explain the default return value of such a function.
 
-# Explanation:
+# Explanation:  If a function does not use a return statement, Python automatically returns None. The function only prints the message and does not return any value.
+
+def message():
+    print("Divya")
+
+message()
